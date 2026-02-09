b52cfbaa6d0d6bbe65d1fbe49c5d515f4b620b12
Check <check@Checks-MacBook-Pro.local>
2026-02-09 17:33:37 +0700

    cell03

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,8 @@
+a = input("What you gotta say? : ")
+while a != "STOP":
+    a = input("I got that! Anything else? : ")
+
+# What you gotta say? : Hello
+# I got that! Anything else? : I like ponies
+# I got that! Anything else? : stop..
+# I got that! Anything else? : STOP
